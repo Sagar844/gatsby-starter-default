e38221d783dce53cbf0eb3dbb25b5b5496123ba5
SaGaR KuMaR <sagarkumar863081@gmail.com>
2023-03-25 13:16:54 +0530

    f

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,16 @@ import Contact from "./Contact/contact"
 import Customer from "./CustomersReview/customer"
 import Doctorcart from "./DoctorCart/doctorcart"
 import Footer from "./footer"
-import Navbar from "../pages/navbar"
-import Services from "./OurServices"
+// import Navbar from "../pages/navbar"
+// import Services from "./OurServices"
 
 export default function Home() {
   return (
     <>
       <div>
-        <Navbar />
+        {/* <Navbar /> */}
         <Bestspecialist />
-        <Services />
+        {/* <Services /> */}
         <Clinic />
         <Doctorcart />
         <Customer />
